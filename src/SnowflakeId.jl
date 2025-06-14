1916a2790463ef7a04f506f4ac18499bbea4807d
Darryl Gamroth <dgamroth@rubus.ca>
2025-06-14 11:16:07 -0700

    Add type parameter Clock to struct

--- src/SnowflakeId.jl
+++ src/SnowflakeId.jl
@@ -4,18 +4,26 @@ using Clocks
 using Hwloc
 using LibUV_jll
 
-export SnowflakeIdGenerator, node_id, timestamp_offset_ms, max_node_id, max_sequence, next_id, extract_timestamp, extract_node_id, extract_sequence
+export SnowflakeIdGenerator,
+    node_id,
+    timestamp_offset_ms,
+    max_node_id,
+    max_sequence,
+    next_id,
+    extract_timestamp,
+    extract_node_id,
+    extract_sequence
 
 const CACHE_LINE_SIZE::Int = maximum(cachelinesize())
 const CACHE_LINE_PAD::Int = CACHE_LINE_SIZE - sizeof(Int64)
 # Number of bits used for the timestamp, allowing for 69 years from `timestamp_offset_ms()`.
-const EPOCH_BITS = 41
+const EPOCH_BITS::Int = 41
 # Total number of bits used to represent the distributed node and the sequence within a millisecond.
-const MAX_NODE_ID_AND_SEQUENCE_BITS = 22
+const MAX_NODE_ID_AND_SEQUENCE_BITS::Int = 22
 # Default number of bits used to represent the distributed node or application, allowing for 1024 nodes (0-1023).
-const NODE_ID_BITS_DEFAULT = 10
+const NODE_ID_BITS_DEFAULT::Int = 10
 # Default number of bits used to represent the sequence within a millisecond, supporting 4,096,000 ids per second per node.
-const SEQUENCE_BITS_DEFAULT = 12
+const SEQUENCE_BITS_DEFAULT::Int = 12
 
 """
     Generate unique identifiers based on the Twitter
@@ -26,7 +34,7 @@ This implementation is lock-less resulting in greater throughput plus less conte
 !!! note
     ntpd, or alternative clock source, should be setup correctly to ensure the clock does not go backwards.
 """
-mutable struct SnowflakeIdGenerator
+mutable struct SnowflakeIdGenerator{C<:AbstractClock}
     const pad1::NTuple{CACHE_LINE_PAD,Int8}
     @atomic timestamp_sequence::Int64
     const pad2::NTuple{CACHE_LINE_PAD,Int8}
@@ -36,9 +44,14 @@ mutable struct SnowflakeIdGenerator
     const max_sequence::Int64
     const node_bits::Int64
     const timestamp_offset_ms::Int64
-    const clock::AbstractClock
+    const clock::C
 
-    function SnowflakeIdGenerator(node_id_bits::Int, sequence_bits::Int, node_id::Int64, timestamp_offset_ms::Int64, clock::AbstractClock)
+    function SnowflakeIdGenerator(node_id_bits::Int,
+        sequence_bits::Int,
+        node_id::Int64,
+        timestamp_offset_ms::Int64,
+        clock::C) where {C<:AbstractClock}
+
         if node_id_bits < 0
             throw(ArgumentError("must be >= 0: node_id_bits=$node_id_bits"))
         end
@@ -68,7 +81,7 @@ mutable struct SnowflakeIdGenerator
         max_sequence = 2^sequence_bits - 1
         node_bits = node_id << sequence_bits
 
-        new(ntuple(x -> Int8(0), CACHE_LINE_PAD),
+        new{C}(ntuple(x -> Int8(0), CACHE_LINE_PAD),
             0,
             ntuple(x -> Int8(0), CACHE_LINE_PAD),
             node_id_and_sequence_bits,
@@ -83,7 +96,6 @@ end
 
 SnowflakeIdGenerator(node_id::Int64) = SnowflakeIdGenerator(NODE_ID_BITS_DEFAULT, SEQUENCE_BITS_DEFAULT, node_id, 0, EpochClock())
 SnowflakeIdGenerator(node_id::Int64, clock::AbstractClock) = SnowflakeIdGenerator(NODE_ID_BITS_DEFAULT, SEQUENCE_BITS_DEFAULT, node_id, 0, clock)
-
 
 """
     node_id(g::SnowflakeIdGenerator) -> Int
